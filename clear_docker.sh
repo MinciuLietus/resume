5a266cfe725f658ccd4601970cb0807c5fba34fa
MinciuLietus <mic.auris6@gmail.com>
2023-02-17 08:26:48 +0200

    remove unused files

--- clear_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker-compose down
-docker rm -f $(docker ps -a -q)
-docker volume rm $(docker volume ls -q)
--- restart_services.sh
+++ restart_services.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-
 docker-compose -f docker-compose.deploy.yml down
 docker-compose -f docker-compose.deploy.yml up
